80fc7d80b811c1b2e570b702750b4889adbbcbca
Alexander Kuzmin <roosit@abricos.org>
2011-05-04 18:02:36 +0000

    Социальный движок: набросок модуля

---
+++ module.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+ * Модуль Social
+ * 
+ * @version $Id: module.php 913 2011-02-28 07:09:11Z roosit $
+ * @package Abricos 
+ * @subpackage Social
+ * @copyright Copyright (C) 2011 Abricos All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ * @author Alexander Kuzmin (roosit@abricos.org)
+ */
+
+$mod = new SocialModule();
+CMSRegistry::$instance->modules->Register($mod);
+
+class SocialModule extends CMSModule {
+	
+	/**
+	 * Конструктор
+	 */
+	public function __construct(){
+		// Версия модуля
+		$this->version = "0.1";
+		
+		// Название модуля
+		$this->name = "social";
+	}
+}
+
+?>
